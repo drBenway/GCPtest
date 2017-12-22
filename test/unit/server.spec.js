2679fcf8de443c31e2e6f18c59bd099e4bc2c8ab
drBenway <yves@westworld.be>
2017-12-22 21:21:19 +0100

    first commit: setup test sever to deploy on GCP

---
+++ test/unit/server.spec.js
@@ -0,0 +1,8 @@
+let expect = require('chai').expect;
+let app = require('../../server');
+describe('<server>',function(){
+  it('should pass',function(){
+    expect(true).to.equal(true);
+  });
+
+});
